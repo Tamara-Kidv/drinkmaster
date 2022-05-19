cb85943576b9376fcb4d3ecda360d90eda994ec2
Jeffrey Roossien <jr.roossien@gmail.com>
2022-05-19 15:19:05 +0200

    Add first page, and navigation to next page.

--- DrinkMaster/App.xaml.cs
+++ DrinkMaster/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace DrinkMaster;
+﻿using DrinkMaster.Pages;
+
+namespace DrinkMaster;
 
 public partial class App : Application
 {
@@ -6,6 +8,7 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		//MainPage = new AppShell();
+		MainPage = new NavigationPage(new StartPage());
 	}
 }
---
+++ DrinkMaster/Pages/StartPage.xaml.cs
@@ -0,0 +1,16 @@
+namespace DrinkMaster.Pages;
+
+public partial class StartPage : ContentPage
+{
+	public StartPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnStartClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainPage()); //Change to your page.
+    }
+
+
+}
